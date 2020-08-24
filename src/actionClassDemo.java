2f8f8adde7d54af70ab4b5381fc0b868dabc98e2
oishyjahedy <oishy.jahedy@gmail.com>
2020-08-24 11:41:17 -0400

    actionClassDemo has how to move mouse to specefic elements, write in cap, right click

--- src/actionClassDemo.java
+++ src/actionClassDemo.java
@@ -1,8 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class actionClassDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\QAT\\selenium-drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");
+		Actions a = new Actions(driver);
+		WebElement move = driver.findElement(By.xpath("//span[@class='nav-line-2 nav-long-width']"));
+		WebElement input = driver.findElement(By.id("twotabsearchtextbox"));
+		
+		
+		a.moveToElement(input).click().keyDown(Keys.SHIFT).sendKeys("world").doubleClick().build().perform();
+		//move mouse to specific elements and context-click help to perform right click.
+		a.moveToElement(move).contextClick().build().perform();
 
 	}
 
